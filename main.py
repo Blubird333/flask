fba9a3ac62aca1832e2660f1955944a1c7773773
Chinthakunta Raj Dheeraj Reddy <rajdheerajcreddy@gmail.com>
2022-06-28 18:33:01 +0530

    Simplest webpage.

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask("Job site")
+
+@app.route("/")                       #decorater(mainly used to make this python file as a flask file.)
+def hello():
+    return "Hi! I'm here!\n"
+
+
+#Steps to run this for flask:
+#save this file.
+#type 'export FLASK_APP=main' and hit enter in the terminal.
+#type 'flask run' and hit enter in the terminal.
